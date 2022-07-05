371c8a7b503a351cfa8c33b686b8c408c7991080
Hailu Worku <hailuworku1@yahoo.com>
2022-07-05 14:00:05 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,10 @@ def read_file(filename=""):
     Args:
         filename (str): filename/filepath
     """
-    if type(filename) is str:
-        with open(filename, 'r') as fyle:
-            print(fyle.read(), end='')
+    if type(filename) is str:       
+        with open(filename, 'r') as reader:
+            # Read and print the entire file line by line
+            line = reader.readline()
+            while line != '':  # The EOF char is an empty string
+                print(line, end='')
+                line = reader.readline()
